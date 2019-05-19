74f09e5d0f7a5f27bfb0f324bdc2ee8440b9313e
theeinsaneone <adhall1811@gmail.com>
2019-05-19 20:16:53 +0530

    Initial Commit

---
+++ safeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  safeApp
+//
+//  Created by Abhishek Dhall on 19/05/19.
+//  Copyright © 2019 Abhishek Dhall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
